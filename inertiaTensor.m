d3918ad1f4a0e7812b8db47a9a3fb2235c3a887a
terminator0010 <34940921+terminator0010@users.noreply.github.com>
2024-07-29 12:23:39 -0300

    Delete inertiaTensor.m

--- inertiaTensor.m
+++
@@ -1,7 +0,0 @@
-function iT = inertiaTensor(params)
-
- x = Ixx*p_dot - Ixz*r_dot - q*(Ixz*p - Izz*r) - Iyy*q*r;
- y = Iyy*q_dot + p*(Ixz*p - Izz*r) + r*(Ixx*p - Ixz*r);
- z = Izz*r_dot - Ixz*p_dot - q*(Ixx*p - Ixz*r) + Iyy*p*q;
-iT = [x;y;z];
-end
