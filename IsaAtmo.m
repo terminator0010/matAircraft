7448167964660f53aef86761bd2752e7c75130ae
terminator0010 <34940921+terminator0010@users.noreply.github.com>
2024-07-31 12:38:56 -0300

    Update IsaAtmo.m

--- IsaAtmo.m
+++ IsaAtmo.m
@@ -25,10 +25,41 @@ function [t1, spSound_mps, pActual, Mach, cdP, CAS] = IsaAtmo(params)
 
   perfGasEq = pActual*R*t1;
 
-  Mach = TAS/spSound_mps;
-
   cdP = pActual*((((((gamma-1)/2)*Mach^2+1)^(gamma/(gamma-1))))-1);
 
-  CAS = sqrt((((2*spSound_mps^2)/(gamma-1))*((cdP/P0)+1)^((gamma-1)/gamma))-1);
+%Alpha_angle = atan2(w/u)
+%Beta_angle = sin^-1(v/Vinf)
+
+%Vinf = sqrt(u²+v²+w²)
+
+%Wind_effect
+%Input
+%LBE
+%BodyVelocities
+%InertialWind_mps(x,y,z)
+
+%Output
+%BodyWind_mps
+%WindSpeed_mps
+%TrueAirSpeed_mps
+%Beta_angle
+%Alpha_angle
+
+
+LEB = transpose(LBE);
+
+Bodywind_mps = LEB*InertialWind_mps;
+
+WindSpeed_mps = BodyVelocities+Bodywind_mps;
+
+TAS_mps = sqrt(WindSpeed_mps(1,1)^2 + WindSpeed_mps(1,2)^2 + WindSpeed_mps(1,3)^2);
+
+Mach = TAS/spSound_mps;
+
+Beta_angle = asin(WindSpeed_mps(1,2)/TASpeed_mps)*(180/pi);
+
+Alpha_angle = atan2(WindSpeed_mps(1,3)/WindSpeed_mps(1,1))*(180/pi);
+
+CAS = sqrt((((2*spSound_mps^2)/(gamma-1))*((cdP/P0)+1)^((gamma-1)/gamma))-1);
 
   end
